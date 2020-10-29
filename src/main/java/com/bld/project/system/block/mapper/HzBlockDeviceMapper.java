2c372a7c3475af41595b64f8f32f9eb40c2c0e2d
huangzhi <1049729282@qq.com>
2020-10-29 16:40:26 +0800

    后台mapper新增

---
+++ src/main/java/com/bld/project/system/block/mapper/HzBlockDeviceMapper.java
@@ -0,0 +1,7 @@
+package com.bld.project.system.block.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bld.project.system.block.model.BlockDevice;
+
+public interface HzBlockDeviceMapper extends BaseMapper<BlockDevice> {
+}
