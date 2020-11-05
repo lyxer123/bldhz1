9bf89dea1e3cb7fb8e6562d784f98bd0872a1918
huangzhi <1049729282@qq.com>
2020-11-05 15:39:13 +0800

    后台mapper新增

--- src/main/java/com/bld/project/system/block/service/BlockHashServiceImpl.java
+++ src/main/java/com/bld/project/system/block/service/BlockHashServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
-public class BlockHashServiceImpl implements BlockHashService{
+public class BlockHashServiceImpl implements BlockHashService {
     @Resource
     private BlockHashMapper blockHashMapper;
     @Resource
@@ -28,24 +28,24 @@ public class BlockHashServiceImpl implements BlockHashService{
         String chipId = bh.getChipId();
         String toWallet = bh.getToWallet();
         String fromWallet = bh.getFromWallet();
-        if (!tbUser.isSysAdmin()){
+        if (!tbUser.isSysAdmin()) {
             BlockDevice bd = new BlockDevice(chipId);
             bd.setTenantId(tbUser.getTenantId().getId());
             bd.setPageNum(0);
             bd.setPageSize(10);
-            if (!tbUser.isTenantAdmin()){
+            if (!tbUser.isTenantAdmin()) {
                 bd.setCustomerId(tbUser.getCustomerId().getId());
             }
             List<BlockDevice> select = blockDeviceMapper.select(bd);
-            if (select == null || select.size() < 1){
+            if (select == null || select.size() < 1) {
                 return ResultListInfo.success(null, "", 0, 0, 0);
             }
         }
-        if (StringUtils.isNullString(chipId) && StringUtils.isNullString(toWallet) && StringUtils.isNullString(fromWallet)){
+        if (StringUtils.isNullString(chipId) && StringUtils.isNullString(toWallet) && StringUtils.isNullString(fromWallet)) {
             return ResultListInfo.error("请输出查询参数");
         }
         BlockHash selBh = new BlockHash(chipId, toWallet, fromWallet);
-        selBh.setPageNum((bh.getPageNum()-1)*bh.getPageSize());
+        selBh.setPageNum((bh.getPageNum() - 1) * bh.getPageSize());
         selBh.setPageSize(bh.getPageSize());
         List<BlockHash> select = blockHashMapper.select(selBh);
         Integer integer = blockHashMapper.selectCount(selBh);
@@ -54,13 +54,13 @@ public class BlockHashServiceImpl implements BlockHashService{
 
     @Override
     public ResultListInfo searchTableMoneyData(BlockHash bh) {
-        Map<String,Object> map = new HashMap<>();
-        bh.setPageNum((bh.getPageNum()-1)*bh.getPageSize());
+        Map<String, Object> map = new HashMap<>();
+        bh.setPageNum((bh.getPageNum() - 1) * bh.getPageSize());
         //查詢數據
         List<BlockHash> blockHash = blockHashMapper.searchTableMoneyData(bh);
         //查詢總條數
         Integer integer = blockHashMapper.searchTableMoneyDataCount(bh);
-        map.put("data",blockHash);
-        return ResultListInfo.success(blockHash,"",integer,0,0);
+        map.put("data", blockHash);
+        return ResultListInfo.success(blockHash, "", integer, 0, 0);
     }
 }
